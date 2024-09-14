006167c6a56d68a30caaf32fda88551f7afd3584
Andrew Hausler <81341702+andrewhausler@users.noreply.github.com>
2024-09-14 11:20:13 -0400

    2:32:25 - Flash Messages

--- base/views.py
+++ base/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.db.models import Q
+from django.contrib.auth.models import User
 from .models import Room, Topic
 from .form import RoomForm
 
-rooms = [
-    {"id":1, "name":"Lets learn python!"},
-    {"id":2, "name":"Design with me"},
-    {"id":3, "name":"Frontend developers"},
-]
+def loginPage(request):
+    username = ""
+    password = ""
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+    context = {"username": username, "password": password}
+    return render(request, "base/login_register.html", context)
 
 def home(request):
     q = request.GET.get("q") if request.GET.get("q") != None else ""
