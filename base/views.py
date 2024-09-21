38cb4c5355635d2021e1f6a822f5245ca13bdc92
Andrew Hausler <81341702+andrewhausler@users.noreply.github.com>
2024-09-21 19:24:17 -0400

    5:11:50 - Edit User Account Page

--- base/views.py
+++ base/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 from .models import Room, Topic, Message
-from .form import RoomForm
+from .form import RoomForm, UserForm
 from django.contrib.auth.forms import UserCreationForm
 
 def logoutUser(request):
@@ -159,3 +159,17 @@ def deleteMessage(request, pk):
         message.delete()
         return redirect("home")
     return render(request, "base/delete.html", {"obj": message})
+
+@login_required(login_url="login")
+def updateUser(request):
+    user = request.user
+    form = UserForm(instance=user)
+
+    if request.method == "POST":
+        form = UserForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect('user-profile', pk=user.id)
+
+    context = {'form': form}
+    return render(request, "base/update-user.html", context)
